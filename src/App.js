146077fbdf4524abf2c2397c1c7be03d8d9f2946
AliRadmanesh <ali.radmanesh1995@gmail.com>
2022-04-23 16:59:38 +0430

    Config project to deploy on github pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Router from "./routes/Router";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
