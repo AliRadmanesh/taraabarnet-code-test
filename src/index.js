cdb1e3c3980112220a04bc1a28d188fba354a424
AliRadmanesh <ali.radmanesh1995@gmail.com>
2022-04-23 16:23:20 +0430

    Configure redux store, saga and persist middlewares

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import rootReducer from "./stores/reducers";
+import rootSaga from "./stores/sagas/rootSaga";
+import reportWebVitals from "./reportWebVitals";
+
+import "./index.css";
+
+const persistConfig = {
+  key: "cards",
+  storage,
+};
+
+const reducer = persistReducer(persistConfig, rootReducer);
+
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
+
+const store = configureStore({
+  reducer,
+  middleware,
+});
+
+const persistor = persistStore(store);
+
+sagaMiddleware.run(rootSaga);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
