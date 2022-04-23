e4a2ad7262012a35a880847835e878eab50ed09c
AliRadmanesh <ali.radmanesh1995@gmail.com>
2022-04-23 17:16:16 +0430

    Change request url to use in gh-pages

--- src/stores/sagas/requests/fetchCardsRequest.js
+++ src/stores/sagas/requests/fetchCardsRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:8585/cards";
+// const url = "http://localhost:8585/cards";
+const url = "https://api.npoint.io/ea6d7a0a1210e646c756";
 
 const fetchCards = () => {
   return axios
