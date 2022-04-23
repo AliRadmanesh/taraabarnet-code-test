fbfd85de849bbcb30bbd06f74adea896b8c28611
AliRadmanesh <ali.radmanesh1995@gmail.com>
2022-04-23 20:42:31 +0430

    Add PWA configs

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import { persistStore, persistReducer } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import App from "./App";
 import rootReducer from "./stores/reducers";
@@ -45,9 +45,12 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-serviceWorkerRegistration.register();
